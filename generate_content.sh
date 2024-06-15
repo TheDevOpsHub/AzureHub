7ec84a4cf2920c81ff78df5c14a6cd296ad1a3bc
Tung Bui <tung.bquang@gmail.com>
2024-06-15 12:15:33 +0700

    Move az service to single file

--- generate_content.sh
+++ generate_content.sh
@@ -8,31 +8,31 @@ services_count=$(jq '.services | length' az_services.json)
 echo "Total services supported: $services_count"
 
 # Save text below <!-- Learning-Resource-End --> to a temporary file
-awk '/<!-- Learning-Resource-End -->/{p=1; next} p' README.md >temp.txt
+awk '/<!-- Learning-Resource-End -->/{p=1; next} p' Azure_Services.md >temp.txt
 echo "Save text below <!-- Learning-Resource-End --> to a temporary file"
 cat temp.txt
 
 echo ""
 echo "Creating new content"
 # Clear all text between "<!-- Learning-Resource-Begin -->" and "<!-- Learning-Resource-End -->"
-sed -i '/<!-- Learning-Resource-Begin -->/,$d' README.md
-# cat README.md
+sed -i '/<!-- Learning-Resource-Begin -->/,$d' Azure_Services.md
+# cat Azure_Services.md
 
 # Append main section header
-echo "<!-- Learning-Resource-Begin -->" >>README.md
-echo "<!-- Do not edit the above line manually -->" >>README.md
-echo "## Azure Services Learning Resources 📘" >>README.md
+echo "<!-- Learning-Resource-Begin -->" >>Azure_Services.md
+echo "<!-- Do not edit the above line manually -->" >>Azure_Services.md
+echo "## Azure Services Learning Resources 📘" >>Azure_Services.md
 # Brief introduction
-echo "This section provides links to detailed documentation, introduction videos, and FAQs for popular AWS services" >>README.md
-echo "- **Total services covered:** **${services_count}**" >>README.md
-echo "- **Azure Docs:** Official documentation for each service" >>README.md
-echo "- **Azure FAQs:** Frequently asked questions about Azure services" >>README.md
+echo "This section provides links to detailed documentation, introduction videos, and FAQs for popular AWS services" >>Azure_Services.md
+echo "- **Total services covered:** **${services_count}**" >>Azure_Services.md
+echo "- **Azure Docs:** Official documentation for each service" >>Azure_Services.md
+echo "- **Azure FAQs:** Frequently asked questions about Azure services" >>Azure_Services.md
 
-echo "" >>README.md
-echo "Full Azure services learning resource table:" >>README.md
-echo "" >>README.md
-echo "| ID | Service Name | Azure Docs | Azure FAQs |" >>README.md
-echo "|----|--------------|----------|----------|" >>README.md
+echo "" >>Azure_Services.md
+echo "Full Azure services learning resource table:" >>Azure_Services.md
+echo "" >>Azure_Services.md
+echo "| ID | Service Name | Azure Docs | Azure FAQs |" >>Azure_Services.md
+echo "|----|--------------|----------|----------|" >>Azure_Services.md
 
 # Initialize ID counter
 id=1
@@ -49,25 +49,25 @@ for service in $services; do
   service_name=$(_jq '.service_name')
   faq_url=$(_jq '.faq_url')
 
-  # Update the service README.md content
-  # echo "| $id | $service_name | 📖 [$service_short_name]($url) | ❔ [$service_short_name/faqs]($faq_url)|" >>README.md
-  echo "| $id | $service_name | 📖 [$service_short_name]($url) | ❔ TODO |" >>README.md
+  # Update the service Azure_Services.md content
+  # echo "| $id | $service_name | 📖 [$service_short_name]($url) | ❔ [$service_short_name/faqs]($faq_url)|" >>Azure_Services.md
+  echo "| $id | $service_name | 📖 [$service_short_name]($url) | ❔ TODO |" >>Azure_Services.md
 
   # Increment ID
   ((id++))
 done
 
-echo "" >>README.md
-echo "And **more upcoming services content...⏩** you can star/follow this repository to get more up-to-dated content ⭐" >>README.md
+echo "" >>Azure_Services.md
+echo "And **more upcoming services content...⏩** you can star/follow this repository to get more up-to-dated content ⭐" >>Azure_Services.md
 
-echo "<!-- Do not edit the below line manually -->" >>README.md
-echo "<!-- Learning-Resource-End -->" >>README.md
+echo "<!-- Do not edit the below line manually -->" >>Azure_Services.md
+echo "<!-- Learning-Resource-End -->" >>Azure_Services.md
 
 # Append the saved text back to the end of the file
-cat temp.txt >>README.md
+cat temp.txt >>Azure_Services.md
 
 # Remove the temporary file
 rm temp.txt
 
-echo "Check the new content in README.md"
-cat README.md
+echo "Check the new content in Azure_Services.md"
+cat Azure_Services.md
